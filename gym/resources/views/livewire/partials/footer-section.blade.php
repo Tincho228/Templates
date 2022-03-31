e31d86fcd9a199ae4189b558bd27f4aac4a2abea
Tincho228 <martin_quintero521@hotmail.com>
2022-03-30 22:29:40 -0300

    commit

--- gym/resources/views/livewire/partials/footer-section.blade.php
+++ gym/resources/views/livewire/partials/footer-section.blade.php
@@ -1,16 +1,36 @@
-<div class="container-fluid pt-5 pb-5 bg-skyblue-middle text-light">
-    <img src="assets/logos/logo.png" alt="Logo" style="display:block; margin:auto;">
-    <p class=" mt-5 mb-3 h5">Veni a visitarnos</p>
-    <div class="footer-contact">
-        <p ><i class="fas fa-map-marker-alt"></i> Avenida 256, San rafael Mendoza</p>
-        <p ><i class="fas fa-envelope"></i> consultas@mygym.com</p>
-        <p ><i class="fas fa-check-circle"></i> Abierto de Lunes a Viernes de 8 a 18hs</p>
+<div class="container-fluid p-3 pb-5 bg-skyblue-middle text-light">
+    <img src="assets/logos/logo.png" alt="Logo" style="display:block; margin:30px auto;">
+    <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-4 footer-contact">
+                <p class=" mt-3 mb-2 h5">Veni a visitarnos</p>
+                <div class="contact-container p-3">
+                    <p><i class="fas fa-map-marker-alt"></i> Avenida 256, San rafael Mendoza</p>
+                    <p><i class="fas fa-envelope"></i> consultas@mygym.com</p>
+                    <p><i class="fas fa-check-circle"></i> Abierto de Lunes a Viernes de 8 a 18hs</p>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-4 footer-social">
+                <p class="mt-3 mb-2 h5">Redes Sociales</p>
+                <div class="social-container p-3">
+                    <a href=""><i class="fab fa-instagram h1 text-light" style="margin-right:10px;"></i></a>
+                    <a href=""><i class="fab fa-facebook-f h1 text-light" style="margin-right:10px;"></i></a>
+                    <a href=""><i class="fab fa-whatsapp h1 text-light"></i></a>
+                </div>
+
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 footer-links">
+                <p class="mt-3 mb-2 h5">Links útiles</p>
+                <div class="p-3 links-container">
+                    <a href="" class="nav-link text-light"><i class="fas fa-check-circle"></i> Acerca de mi</a>
+                    <a href="" class="nav-link text-light"><i class="fas fa-check-circle"></i> Contacto</a>
+                    <a href="" class="nav-link text-light"><i class="fas fa-check-circle"></i> Home</a>
+                </div>
+            </div>
+        </div>
     </div>
-    <p class="mt-5 mb-3 h5">Redes Sociales</p>
-    <div class="footer-links">
-        <i class="fab fa-instagram h1" style="margin-right:10px;"></i>
-        <i class="fab fa-facebook-f h1" style="margin-right:10px;"></i>
-        <i class="fab fa-whatsapp h1"></i>
-    </div>
-    
+    <hr>
+    <p class="text-center text-light">Todos los derechos reservados | mygym.com</p>
 </div>
