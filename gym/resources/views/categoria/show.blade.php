47bda33d48deeb5148c815d0aaf813f37372f387
Tincho228 <qui18025@byui.edu>
2022-04-05 15:12:42 -0300

    commit

--- gym/resources/views/categoria/show.blade.php
+++ gym/resources/views/categoria/show.blade.php
@@ -1,3 +1,96 @@
 <x-app-layout>
-    <h1>Hola {{$categoria->name}}</h1>
+    <div class="container-fluid" style="background-image:url('{{URL::asset('assets/images/call-to-action.jpg')}}'); background-repeat:no-repeat; background-size:cover;background-position:center;">
+    <div class="container pt-5 pb-5">
+        <div class="card">
+            <div class="card-body">
+                <div class="row mb-3">
+
+                    <div class="col-sm-12 col-md-6">
+                        <div class="img-wrapper mb-2 mt-2">
+                            @isset($categoria->image)
+                            @if($categoria->image->url == 'placeholder')
+                            <img id="picture" class="img-fluid"
+                                src="https://images.pexels.com/photos/669582/pexels-photo-669582.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                                alt="">
+                            @else
+                            <img id="picture" src="{{Storage::url($categoria->image->url)}}" class="img-fluid"
+                                alt="Imagen previa">
+                            @endif
+                            @else
+                            <img id="picture" class="img-fluid"
+                                src="https://images.pexels.com/photos/669582/pexels-photo-669582.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                                alt="">
+                            @endisset
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <h1 class="title-font">{{$categoria->name}}</h1>
+                        <hr>
+                        <p class="h4">Descripcion:</p>
+                        <p>{{$categoria->description}}</p>
+                        <hr>
+                        <p class="h4">Precio:</p>
+                        <p class="h1">${{$categoria->price}}</p>
+                        <hr>
+                        <div class="d-flex justify-content-end">
+                            <a class="btn btn-white" href="{{ route('home.index') }}">Volver</a>    
+                            <a class="btn btn-yellow" href="">Solicitar</a>
+                        </div>
+                        <div class="detail-new-product mt-5">
+                            @livewire('home.new-product-section')
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <h3>Galeria</h3>
+                <div class="d-flex">
+                    <div class="item-detail"><img class="img-fluid" src="{{URL::asset('assets/images/new-product.jpg')}}" alt=""></div>
+                    <div class="item-detail"><img class="img-fluid" src="{{URL::asset('assets/images/new-product.jpg')}}" alt=""></div>
+                    <div class="item-detail"><img class="img-fluid" src="{{URL::asset('assets/images/new-product.jpg')}}" alt=""></div>
+                    <div class="item-detail"><img class="img-fluid" src="{{URL::asset('assets/images/new-product.jpg')}}" alt=""></div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+<div>
+
+</div>
+<style>
+    .item-detail {
+        width:22%;
+        margin:5px;
+    }
+    .img-wrapper img {
+        border-radius: 5px;
+    }
+    .card {
+        background-color:#ffffff57;
+        color:whitesmoke;
+    }
+    .btn-white {
+        color:whitesmoke;
+        border-radius: 25px;
+        padding:5px 20px;
+        border:1px solid whitesmoke;
+        margin-right:10px;
+    }
+    .btn-white:hover {
+        background-color:whitesmoke;
+        color:black;
+    }
+    .btn-yellow:hover {
+        background-color:yellow;
+        color:black !important;
+    }
+    .detail-new-product {
+        overflow: hidden;
+    }
+    .new-product-price {
+        width:20%;
+        right:50px;
+    }
+</style>
+
 </x-app-layout>
--- gym/resources/views/livewire/home/new-product-section.blade.php
+++ gym/resources/views/livewire/home/new-product-section.blade.php
@@ -1,7 +1,7 @@
 <div class="new-product-section">
-    <img src="assets/images/new-product.jpg" class="new-product-image" alt="new product">
+    <img src="{{URL::asset('assets/images/new-product.jpg')}}" class="new-product-image" alt="new product">
     <div class="new-product-price">
-        <img src="assets/images/new-product-price.png" alt="new product price">
+        <img src="{{URL::asset('assets/images/new-product-price.png')}}" alt="new product price">
     </div>
     <a class="btn new-product-button btn-yellow btn-sm blink_me" href="">Comenzar</a>
 </div>
