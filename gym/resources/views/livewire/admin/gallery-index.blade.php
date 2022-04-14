5d8c37c94614a00ccdb7e5b3184e24ed0c60ab8c
Tincho228 <qui18025@byui.edu>
2022-04-14 15:42:59 -0300

    commit

--- gym/resources/views/livewire/admin/gallery-index.blade.php
+++ gym/resources/views/livewire/admin/gallery-index.blade.php
@@ -64,9 +64,14 @@
                             <img class="img-fluid mb-4" src="{{$image->temporaryUrl()}}" alt="">
                         @endif
                         
+                        <!--Small Caption-->
+                        <div>
+                            <img id="output">    
+                        </div>    
+
                         <div class="form-group">
                             <label for="image">Elegir una foto</label>
-                            <input type="file" class="form-control-file" id="image" wire:model="image">
+                            <input type="file" class="form-control-file" id="image" wire:model="image" onload="process()">
                         </div>
                         @error('image')
                             <div class="text-danger mb-4">{{$message}}</div>
@@ -84,6 +89,17 @@
         window.addEventListener('closeModal', event => {
             $("#exampleModal").modal('hide');                
         })
+        function process(){
+        const file = document.querySelector("#image").files;
+        console.log(file)
+        // const reader = new FileReader();
+        // reader.readAsDataURL(file);
+        // reader.onload = function (event) {
+        //         const imgElement = document.createElement('img');
+        //         imgElement.src = event.target.result;
+        //         document.querySelector("#output").src = event.target.result;
+        // }
+        }
     </script>
     <style>
     .lds-roller {
