8cf3832916d99418d6caa5b4b6e0fbe177850aba
Tincho228 <qui18025@byui.edu>
2022-05-03 10:52:43 -0300

    membresia

--- gym/public/js/countdown.js
+++ gym/public/js/countdown.js
@@ -1,9 +1,37 @@
+/* Obtaining values from the user */
+var dateString = document.getElementById('counter').dataset.date;
+var planString = document.getElementById('counter').dataset.plan;
+
+
+var plan = parseInt(planString) /* Expiring plan - 6 months*/
+console.log
+var info = dateString.split('-');
+var year_created = parseInt(info[0]);
+var month_created = parseInt(info[1]);
+var day_created = parseInt(info[2]);
+var day_expiration, month_expiration,year_expiration;
+
+/* Expiring plan - 6 months*/
+if ((month_created+plan)>12){
+    month_expiration = month_created+plan-12
+    year_expiration = year_created + 1
+}else {
+    month_expiration = month_created + plan
+    year_expiration = year_created    
+}
+
+day_expiration = day_created
+
+//var month_expiration = month_created + plan
+//var year_expiration = year_created
+
 simplyCountdown('#counter', {
-    year: 2022, // required
-    month: 6, // required
-    day: 28, // required
-    hours: 13, // Default is 0 [0-23] integer
-    minutes: 0, // Default is 0 [0-59] integer
+    // year: 2022, // required
+    year: year_expiration,
+    month: month_expiration, // required
+    day: day_expiration, // required
+    hours: 10, // Default is 0 [0-23] integer
+    minutes: 31 , // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
         days: { singular: 'Dia', plural: 'dias' },
@@ -16,7 +44,10 @@ simplyCountdown('#counter', {
     inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
     // in case of inline set to false
     enableUtc: false, //Use UTC or not - default : false
-    onEnd: function() { return; }, //Callback on countdown end, put your own function here
+    onEnd: function() {
+        var counter_alert = document.getElementById('counter_alert');
+        counter_alert.style.display = 'block';
+        return }, //Callback on countdown end, put your own function here
     refresh: 1000, // default refresh every 1s
     sectionClass: 'simply-section', //section css class
     amountClass: 'simply-amount', // amount css class
--- gym/resources/views/profile/membership-user.blade.php
+++ gym/resources/views/profile/membership-user.blade.php
@@ -9,21 +9,26 @@
 
     <x-slot name="content">
         <h3 class="h5 font-weight-bold">
-                {{ __('Plan') }}
+                {{ __('Plan Mensual') }} <strong class="text-success">Activo</strong>
         </h3>
 
         <p class="mt-3">
-            {{ __('When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from your phone\'s Google Authenticator application.') }}
+            {{ __('YOGA') }}
         </p>
 
         <div class="mt-3">
             <h3 class="h5 font-weight-bold">
                 {{ __('Duracion de membresia') }}
         </h3>
-        <div id="counter"></div>
-        <p class="mt-3">
-            <strong>Nota: </strong>{{ __('Una vez vencida tu membresia, el administrador puede eliminar tu cuenta pasados los treinta dias.') }}
-        </p>
+        <div id="counter" data-date={{Auth::user()->created_at}} data-plan="1"></div>
+        
         </div>
+        <div id="counter_alert" class="alert alert-danger">
+            <h5 class="font-weight-bold">Su membresia expiro!!!!</h5>
+            <p class="mt-3">
+                <strong>Nota: </strong>{{ __('Una vez vencida tu membresia, el administrador puede eliminar tu cuenta pasados los treinta dias.') }}
+            </p>
+        </div>
+       
     </x-slot>
 </x-jet-action-section>
